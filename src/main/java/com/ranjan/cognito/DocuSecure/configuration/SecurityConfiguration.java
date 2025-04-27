9b8c4f7b17212e4ddaf9d0135b6556c6f77c2f2b
ranjan <ranjan@example.com>
2025-04-27 10:23:03 +0100

    cors

--- src/main/java/com/ranjan/cognito/DocuSecure/configuration/SecurityConfiguration.java
+++ src/main/java/com/ranjan/cognito/DocuSecure/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.ranjan.cognito.DocuSecure.configuration;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +10,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
+
 public class SecurityConfiguration {
      @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,23 +28,38 @@ public class SecurityConfiguration {
         http.csrf(c->c.disable())
             // Enable OAuth2 Login for Thymeleaf frontend
             .oauth2Login(Customizer.withDefaults())
+            .cors(cors->cors.configurationSource(corsConfigurationSource()))
             // Enable OAuth2 Resource Server with JWT validation for the backend API
             .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
             // Configure access rules
             .authorizeHttpRequests((c)->{
-                c.requestMatchers("/","/logged-out", "/error","/swagger-ui.html").permitAll();
+                c.requestMatchers("/","/logged-out", "/error").permitAll();
                 c.requestMatchers(
                                             "/upload",
                                             "/download/**",
                                             "/api/**",
                                             "/custom-logout",
-                                            "/v3/api-docs/**",
-                                            "/swagger-ui/**").authenticated();
+                                            "/swagger-ui/**"
+                                            ,"/v3/api-docs/**"
+                                            ).authenticated();
             })
             .logout(c->c.disable());
                 
                 
                 
         return http.build();
+    }
+
+   @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/ranjan/cognito/DocuSecure/restcontroller/DocRestController.java
+++ src/main/java/com/ranjan/cognito/DocuSecure/restcontroller/DocRestController.java
@@ -15,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
